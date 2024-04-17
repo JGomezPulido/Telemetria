4df9509d0504c6bcf6ea85a47460284aa2caac24
Elena R <74355341+elenrobe@users.noreply.github.com>
2024-04-17 21:23:04 +0200

    serializador a csv inicio

---
+++ Telemetria/Telemetria/SerializerCSV.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Telemetria
+{
+
+    internal class SerializerCSV
+    {
+
+        string csvFilePath = "eventos.csv";
+        public void exportar(List<Event> events)
+        {
+            // Abrir el archivo en modo escritura
+            using (StreamWriter sw = new StreamWriter(csvFilePath))
+            {
+                
+                sw.WriteLine("UserId, SessionID, EventType, Timestamp");
+
+                // Escribir cada evento como una fila en el archivo CSV
+                foreach (Event ev in events)
+                {
+
+                    // Escribir la línea en el archivo
+                    string csvLine = $"{ev.id_user},{ev.id_session},{ev.event_type},{ev.send_time}";
+
+                    sw.WriteLine(csvLine);
+                }
+
+            }
+        }
+    }
+}
