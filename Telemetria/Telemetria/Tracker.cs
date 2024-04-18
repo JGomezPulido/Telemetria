8938da77f963522063ccf99e55e3e3cea1e49bc2
jgomez18UCM <jgomez18@ucm.es>
2024-04-18 15:01:02 +0200

    Eliminados archivos generados xd

---
+++ Telemetria/Telemetria/Tracker.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace Telemetria
+{
+    public class Tracker
+    {
+        private static Tracker? _instance = null;
+
+        public static Tracker? Instance { get { return _instance; } }
+
+        List<Event> eventsList = new List<Event>();
+        List<Event> pendingEvents = new List<Event>();
+        private Tracker()
+        {
+
+        }
+
+        public static void Init()
+        {
+            _instance = new Tracker();
+        }
+    }
+}
