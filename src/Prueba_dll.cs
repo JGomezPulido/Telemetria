76272ceabb1cd55817015b52f73ecf9bc5235e30
Nahia <82345386+nahigles@users.noreply.github.com>
2024-04-18 18:23:28 +0200

    Prueba dll parte1

---
+++ src/Prueba_dll.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telemetria.src
+{
+    public class Prueba_dll
+    {
+
+        public int a;
+        public bool b;
+
+
+        public void Pruebacion(int x, int y)
+        {
+            a = x - y;
+
+            if (a > 15)
+            {
+                b = true;
+            }
+            else
+            {
+                b = false;
+            }
+        }
+    }
+}
