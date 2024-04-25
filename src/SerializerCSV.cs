cadeb5be5361a451adf1b4f32f4db2b561663a9b
jgomez18UCM <jgomez18@ucm.es>
2024-04-25 16:50:48 +0200

    Arreglado el proyecto

--- src/SerializerCSV.cs
+++ src/SerializerCSV.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Telemetria;
+using System.IO;
 
 namespace Telemetria
 {
--- src/Tracker.cs
+++ src/Tracker.cs
@@ -4,8 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Collections;
+using System.Threading;
 using System.Collections.Concurrent;
-using UnityEngine;
 
 namespace Telemetria
 {
